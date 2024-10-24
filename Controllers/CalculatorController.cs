29e932f1115745261974ab1590d8265463525139
nastyaan <125795702+nastyaan@users.noreply.github.com>
2024-10-24 16:57:33 +0500

    Добавьте файлы проекта.

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2_Calculator.Controllers
+{
+    public enum Operation { Add, Subtract, Multiply, Divide }
+
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Calculate(double num1, double num2, Operation operation)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case Operation.Add:
+                    result = num1 + num2;
+                    break;
+                case Operation.Subtract:
+                    result = num1 - num2;
+                    break;
+                case Operation.Multiply:
+                    result = num1 * num2;
+                    break;
+                case Operation.Divide:
+                    result = num1 / num2;
+                    break;
+            }
+            ViewBag.Result = result;
+            return View("Index");
+        }
+    }
+}
